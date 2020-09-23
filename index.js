e842223982e21711b6cb971984ada34764fdb3cf
culturestate <shane@shanecole.com>
2020-09-23 23:14:50 +0800

    Added write mode

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const Logger = {
 
 module.exports = {
 
-    onPreBuild: ({ inputs, utils: { build: { failPlugin, failBuild } } }) => {
-	try {
+    onPreBuild: ({ constants, inputs, utils: { build: { failPlugin, failBuild } } }) => {
+
 	    // Check inputs
 	    global.verbose = inputs.verbose;
 
@@ -29,40 +29,53 @@ module.exports = {
 		}
 
 		// Prep the necessary variables
-		const redirect_file = getRedirects(env.CONTEXT);
+		const redirect_mode = inputs.mode;
 		const config_file = inputs.config_file;
+		const publish_dir = constants.PUBLISH_DIR;
+		const redirect_file = getRedirects(env.CONTEXT);
 
 		// If there are no redirects to set, bail out before the build starts
 		if (!isValidString(redirect_file)) {
 			return failPlugin('No redirects specified for context: ' + env.CONTEXT);
 		}
 
-		Logger.debug('Using "' + redirect_file + '"" in context "' + env.CONTEXT + '"" with ' + config_file);
-
-		// Read the specified redirects file
-		fs.readFile(redirect_file, function read(err, data) {
-		    if (err) {
-		        return failBuild('Couldn\'t read the specified redirects_file: ' + err);
-		    }
-		    const redirects = '\n\n' + data;
-
-		   	Logger.debug('Preparing to append redirects...');
-		    setRedirects(redirects);
-		});
-
-		// Append the redirects to the end of the specified config file
-		function setRedirects(redirects) {
-			try {
-				Logger.debug('Appending redirects...');
-				fs.appendFileSync(config_file,redirects);
-				Logger.debug('Done.');
-			} catch(err) {
-				return failBuild('Error while appending redirects: ', err);
-			}
+		// If we're using a _redirects file, generate it
+		if (redirect_mode == 'write') {
+			Logger.debug('Write mode: using "' + redirect_file + '" in context "' + env.CONTEXT + '"');
+			fs.copyFile(redirect_file, publish_dir + '/_redirects', (err) => {
+			    if (err) {
+			        return failBuild('Couldn\'t write _redirects: ' + err);
+			    }
+			    Logger.debug('Wrote _redirects successfully.');
+			});
 		}
 
-	} catch {
-		console.log('Something has gone horribly wrong.');
-	}
+		// If we're appending rules to netlify.toml, append 'em
+		if (redirect_mode == 'append') {
+
+			Logger.debug('Append mode: using "' + redirect_file + '" in context "' + env.CONTEXT + '" for ' + config_file);
+
+			// Read the specified redirects file
+			fs.readFile(redirect_file, function read(err, data) {
+			    if (err) {
+			        return failBuild('Couldn\'t read the specified redirects_file: ' + err);
+			    }
+			    const redirects = '\n\n' + data;
+
+			   	Logger.debug('Preparing to append redirects...');
+			    setRedirects(redirects);
+			});
+
+			// Append the redirects to the end of the specified config file
+			function setRedirects(redirects) {
+				try {
+					Logger.debug('Appending redirects...');
+					fs.appendFileSync(config_file,redirects);
+					Logger.debug('Done.');
+				} catch(err) {
+					return failBuild('Error while appending redirects: ', err);
+				}
+			}
+		}
     },
 }
