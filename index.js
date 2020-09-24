b076970d92c27997dbc4d9ea786b1ce43c799c48
culturestate <shane@shanecole.com>
2020-09-24 09:24:05 +0800

    Updated write mode -> standalone

--- index.js
+++ index.js
@@ -32,31 +32,42 @@ module.exports = {
 		const redirect_mode = inputs.mode;
 		const config_file = inputs.config_file;
 		const publish_dir = constants.PUBLISH_DIR;
-		const redirect_file = getRedirects(env.CONTEXT);
+		const redirect_source = getRedirects(env.CONTEXT);
 
-		// If there are no redirects to set, bail out before the build starts
-		if (!isValidString(redirect_file)) {
+		// If no redirects have been specified for this context, bail out before the build starts
+		if (!isValidString(redirect_source)) {
 			return failPlugin('No redirects specified for context: ' + env.CONTEXT);
 		}
 
-		// If we're using a _redirects file, generate it
-		if (redirect_mode == 'write') {
-			Logger.debug('Write mode: using "' + redirect_file + '" in context "' + env.CONTEXT + '"');
-			fs.copyFile(redirect_file, publish_dir + '/_redirects', (err) => {
-			    if (err) {
-			        return failBuild('Couldn\'t write _redirects: ' + err);
-			    }
-			    Logger.debug('Wrote _redirects successfully.');
-			});
+		// If redirects have been specified for this context, verify that the file exists
+		if (!fs.existsSync(redirect_source)) {
+			return failBuild('Couldn\'t find the specified redirects file: ' + redirect_source);
 		}
 
-		// If we're appending rules to netlify.toml, append 'em
+		// If we're in standalone mode, copy the specified redirects to _redirects
+		if (redirect_mode == 'standalone') {
+			
+			Logger.debug('Write mode: using "' + redirect_source + '" in context "' + env.CONTEXT + '"');
+
+			try {
+				fs.copyFile(redirect_source, publish_dir + '/_redirects', (err) => {
+				    if (err) {
+				        return failBuild('Couldn\'t write _redirects: ' + err);
+				    }
+				    Logger.debug('Wrote _redirects successfully.');	
+				});
+			} catch(err) {
+				return failBuild('Error while writing _redirects: ' + err);
+			}
+		}
+
+		// If we're in append mode, read and append the specified redirects to the config file
 		if (redirect_mode == 'append') {
 
-			Logger.debug('Append mode: using "' + redirect_file + '" in context "' + env.CONTEXT + '" for ' + config_file);
+			Logger.debug('Append mode: using "' + redirect_source + '" in context "' + env.CONTEXT + '" for ' + config_file);
 
-			// Read the specified redirects file
-			fs.readFile(redirect_file, function read(err, data) {
+			// Read the redirects file...
+			fs.readFile(redirect_source, function read(err, data) {
 			    if (err) {
 			        return failBuild('Couldn\'t read the specified redirects_file: ' + err);
 			    }
@@ -66,14 +77,14 @@ module.exports = {
 			    setRedirects(redirects);
 			});
 
-			// Append the redirects to the end of the specified config file
+			// ...then append it.
 			function setRedirects(redirects) {
 				try {
 					Logger.debug('Appending redirects...');
 					fs.appendFileSync(config_file,redirects);
 					Logger.debug('Done.');
 				} catch(err) {
-					return failBuild('Error while appending redirects: ', err);
+					return failBuild('Error while appending redirects: ' + err);
 				}
 			}
 		}
